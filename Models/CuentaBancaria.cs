20dc8a688a771e1b304c172e98a14cd2493febdb
JC-Morales03 <juan_morales8@usmp.pe>
2024-04-27 09:05:11 -0500

    Crear Modelo CuentaBancaria

---
+++ Models/CuentaBancaria.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoJC.Models
+{
+    public class CuentaBancaria
+    {
+        public int Id { get; set; }
+    public string NombreTitular { get; set; }
+    public string TipoCuenta { get; set; } 
+    public decimal SaldoInicial { get; set; }
+    public string Email { get; set; }
+    }
+}
