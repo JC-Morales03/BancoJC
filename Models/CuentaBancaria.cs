6f24c00c8d2b2928341b9993e9c7b1e5cc5c5c08
JC-Morales03 <juan_morales8@usmp.pe>
2024-04-27 11:56:46 -0500

    Ultimos cambios

--- Models/CuentaBancaria.cs
+++ Models/CuentaBancaria.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BancoJC.Models
 {
+
+    [Table("t_cuenta")]
     public class CuentaBancaria
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
         [Required]
         [EmailAddress]
